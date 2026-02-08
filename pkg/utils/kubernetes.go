88f0fe00a8a166c7fa8d5bbc822c34c5bccf14dc
Davide Bizzarri <david3bizzarri@proton.me>
2026-02-08 17:59:26 +0100

    Fix lint issues

--- pkg/utils/kubernetes.go
+++ pkg/utils/kubernetes.go
@@ -6,7 +6,10 @@ import (
 )
 
 // FindStatusDeploymentCondition finds the conditionType in conditions.
-func FindStatusDeploymentCondition(conditions []appsv1.DeploymentCondition, conditionType string) *appsv1.DeploymentCondition {
+func FindStatusDeploymentCondition(
+	conditions []appsv1.DeploymentCondition,
+	conditionType string,
+) *appsv1.DeploymentCondition {
 	for i := range conditions {
 		if string(conditions[i].Type) == conditionType {
 			return &conditions[i]
